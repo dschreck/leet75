da6a0cdf5deb86a18766fde42d41dc6cdf5db913
David Schreck <dschreck@gmail.com>
2023-12-16 17:57:13 -0800

    Add solution for 01. Two Sum

---
+++ 01_two-sum/solution.test.ts
@@ -0,0 +1,33 @@
+import { twoSum } from "./solution";
+
+describe("twoSum", () => {
+  it("should return the correct indices", () => {
+    const nums = [2, 7, 11, 15];
+    const target = 9;
+    const expected = [0, 1];
+
+    const actual = twoSum(nums, target);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return the correct indices", () => {
+    const nums = [3, 2, 4];
+    const target = 6;
+    const expected = [1, 2];
+
+    const actual = twoSum(nums, target);
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("should return the correct indices", () => {
+    const nums = [3, 3];
+    const target = 6;
+    const expected = [0, 1];
+
+    const actual = twoSum(nums, target);
+
+    expect(actual).toEqual(expected);
+  });
+});
